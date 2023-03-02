379696dc380fff1028f05630be46829af181e392
zaid <zaedking445@gmail.com>
2023-03-02 15:57:16 +0100

    home page

--- menu-planner/src/App.jsx
+++ menu-planner/src/App.jsx
@@ -1,8 +1,15 @@
 import { useState } from "react";
+import Header from "./components/Header/Header";
 import "./App.scss";
-
+import Home from "./views/Home/Home";
+import Header from "./components/Header/Header";
 function App() {
-  return <div className="App">Hallo</div>;
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+    </div>
+  );
 }
 
 export default App;
--- menu-planner/src/components/Header/Header.jsx
+++ menu-planner/src/components/Header/Header.jsx
@@ -1,7 +1,36 @@
 import React from "react";
-
+import "./Header.scss";
 function Header() {
-  return <div></div>;
+  return (
+    <div>
+      <header className="header">
+        <nav className="nav">
+          <ul className="ul">
+            <li>
+              <a href="/" className="links">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="/Recipies" className="links">
+                Recipies
+              </a>
+            </li>
+            <li>
+              <a href="/User" className="links">
+                Profil
+              </a>
+            </li>
+            <li>
+              <a href="/Contact" className="links">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
 }
 
 export default Header;
