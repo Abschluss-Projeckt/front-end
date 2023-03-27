16c3af829bb91d7c77c7e4627cf2e9b9d19fe042
Rahman E <rahman.erturk@gmail.com>
2023-03-27 16:50:05 +0200

    rating works

--- menu-planner/src/views/RecipePage/RecipePage.jsx
+++ menu-planner/src/views/RecipePage/RecipePage.jsx
@@ -1,52 +1,30 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { useParams, useNavigate } from "react-router-dom";
+
 import axios from "axios";
+
+import { RecipeContext } from "../../contexts/Context";
+
 import "./RecipePage.scss";
 
-function Recipe(props) {
-  return (
-    <div className="recipe">
-      <h2>{props.title}</h2>
-      <div className="img">
-        <img src={props.image} alt={props.title} />
-      </div>
-      <section>
-        <p>{props.explanation}</p>
-        <h3>preparationTime:</h3>
-        <p>{props.preparationTime} min</p>
-        <h3>Ingredients:</h3>
-        <ul>
-          {props?.ingredients?.map((ingredient, index) => (
-            <li key={index}>
-              <div className="amount-M">
-                <p>
-                  {ingredient.amount} {ingredient.measure}
-                </p>
-              </div>
-              <p>{ingredient.name}</p>
-            </li>
-          ))}
-        </ul>
-        <h3>Instructions:</h3>
-        <ol>
-          {props?.description?.map((description, index) => (
-            <li key={index}>{description}</li>
-          ))}
-        </ol>
-      </section>
-    </div>
-  );
-}
-
 function RecipePage() {
+  const { loggedInCookie } = useContext(RecipeContext);
+
   const [recipe, setRecipe] = useState({});
+
+  const [portion, setPortion] = useState(0);
+  const [amount, setAmount] = useState(1);
+
   const { recipeId } = useParams();
   const navigate = useNavigate();
 
   useEffect(() => {
     axios
       .get(`/api/recipes/${recipeId}`)
-      .then((res) => setRecipe(res.data))
+      .then((res) => {
+        setRecipe(res.data);
+        setPortion(res.data?.portion);
+      })
       .catch((err) => console.log(err));
   }, [recipeId]);
 
@@ -57,17 +35,98 @@ function RecipePage() {
       </div>
     );
   }
+
+  const handleMinus = () => {
+    portion > 0 ? setPortion(portion - 1) : setPortion(0);
+  };
+  const handlePlus = () => {
+    setPortion(portion + 1);
+  };
+
+  useEffect(() => {
+    setAmount(portion / recipe.portion);
+  }, [portion]);
+
+  const total = recipe.recipeRanking?.reduce((total, num) => total + num, 0);
+
+  const rating = Math.round(total / recipe.recipeRanking?.length);
+
+  const stars = [];
+  for (let i = 1; i <= 5; i++) {
+    const filled = i <= rating ? "★" : "☆";
+    stars.push(
+      <span key={i} onClick={() => rateRecipe(i)}>
+        {filled}
+      </span>
+    );
+  }
+
+  const rateRecipe = (rating) => {
+    console.log(rating);
+    if (loggedInCookie) {
+      const newValue = recipe.recipeRanking.push(+rating);
+      console.log(recipe.recipeRanking);
+      axios
+        .patch(`/api/recipes/${recipeId}`, {
+          ...recipe,
+          recipeRanking: recipe.recipeRanking,
+        })
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => console.log(err));
+    }
+  };
+
   return (
     <div className="recipe-page">
       <button onClick={() => navigate("/recipes")}>Back</button>
-      <Recipe
-        title={recipe.name}
-        image={recipe.image}
-        explanation={recipe.explanation}
-        ingredients={recipe.ingredients}
-        description={recipe.description}
-        preparationTime={recipe.preparationTime}
-      />
+      <div className="recipe">
+        <div className="img">
+          <img src={recipe.image} alt={recipe.title} />
+        </div>
+        <section>
+          <h2>{recipe.name}</h2>
+          <div className="ranking">{stars}</div>
+          <p>{recipe.explanation}</p>
+          <h3>Preparation Time:</h3>
+          <p>{recipe.preparationTime} min</p>
+
+          <h3>Ingredients:</h3>
+
+          <div className="portion">
+            <p onClick={handleMinus} className="minus">
+              -
+            </p>
+            <p>{portion}</p>
+            <p onClick={handlePlus} className="plus">
+              +
+            </p>
+            <p className="portion_title">
+              {recipe.portion > 1 ? "Portionen" : "Portion"}
+            </p>
+          </div>
+
+          <ul>
+            {recipe?.ingredients?.map((ingredient, index) => (
+              <li key={index}>
+                <div className="amount-M">
+                  <p>
+                    {amount * ingredient.amount} {ingredient.measure}
+                  </p>
+                </div>
+                <p>{ingredient.name}</p>
+              </li>
+            ))}
+          </ul>
+          <h3>Instructions:</h3>
+          <ol>
+            {recipe?.description?.map((description, index) => (
+              <li key={index}>{description}</li>
+            ))}
+          </ol>
+        </section>
+      </div>
     </div>
   );
 }
